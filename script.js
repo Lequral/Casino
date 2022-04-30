59e5db8bed1290c04ca8e9710a089f54b75a9773
Lequral <monsieur.lol@yahoo.com>
2022-04-30 15:55:25 +0200

    Casino update

--- script.js
+++ script.js
@@ -1,119 +1,75 @@
-// Generation des images qui forme le bateau 
+var button = document.getElementById("tourner");
+var isTurning = false;
+var money = 4000;
+const slots = document.querySelector("#slots");
+const cubes = slots.querySelectorAll(".box");
 
-/**@type {HTMLImageElement} */
-var player = document.querySelector("div#player");
-const numb = [9, 8, 2, 5, 5, 20, 20];
-const offsetList = [0, 0, 0, 0, 0, -24, -26, 5];
+function tourner() {
+    console.log("click");
 
-const pathSrc = "./assets/player/boat/B3-";
+    if(isTurning) { //(isTurning === true)
+        isTurning = false;
+        button.textContent = "TOURNER";
 
-function createCopies() {
-    let yIndex = 0;
+        const nb1 = aleatoire();
+        const nb2 = aleatoire();
+        const nb3 = aleatoire();
+        //arrête les cubes sur un face aléatoire
+        cubes.forEach(elem => {
+            elem.style.animationPlayState = "paused";
+        });
 
-    for (let imageIndex = 0; imageIndex < numb.length; imageIndex++) {
-        console.log("nouvelle image");
+        console.log(nb1,nb2,nb3);
+        result(nb1,nb2,nb3);
+    }else {
+        isTurning = true;
+        button.textContent = "ARRETER";
+        
+        //animation des cubes qui tournent à l'infini
+        cubes.forEach(elem => {
+            elem.style.animation = "spin 1s infinite reverse linear";
+        });
+    }
+}
 
-        for (let i = 0; i < numb[imageIndex]; i++) {
+function result(nb1,nb2,nb3) {
+    //0,1,2 → x2        3/8
+    //3,4,5 → +2000     3/8
+    //7     → x10       1/8
+    //7     → +10000    1/8
+    //l'essaie = -1000
+    const faceCorrespondante = [0,0,0,1,1,1,2,3];
+    const nombre1 = faceCorrespondante[nb1];
+    const nombre2 = faceCorrespondante[nb2];
+    const nombre3 = faceCorrespondante[nb3];
 
-            console.log("copie");
-            var elem = document.createElement("img");
-            elem.classList += "boat";
-            elem.src = pathSrc + (imageIndex + 1) + ".svg";
-            elem.offset = numb[imageIndex];
-            elem.style.transform = `translateY(${offsetList[imageIndex] + yIndex}px)`;
-
-            player.append(elem);
-
-            yIndex -= 0.5;
+    money -= 1000;
+    if(nombre1 === nombre2 && nombre2 === nombre3) {
+        console.log("gagne");
+        
+        //bonus
+        if(nombre1 === 0) {
+            money = money * 2;
+        }else if(nombre1 === 1) {
+            money = money + 2000;
+        }else if(nombre1 === 2) {
+            money = money * 10;
+        }else if(nombre1 === 3) {
+            money = money + 10000;
         }
     }
-};
+    setMoneyPara(money);
+}
 
-createCopies();
+function setMoneyPara(val) {
+    const p = document.querySelector("p#argent");
+    p.textContent = val + "$";
+}
 
+function aleatoire() {
+    const min = 0;
+    const max = 7;
+    return Math.round(Math.random() * max + min); //trouver un nombre aléatoire entre 0 et 7
+}
 
-
-// Rotation avec touches q/d + vitesse avec touche z + diminution de la vitesse tt les 1/2 seconde
-
-var rotation = 0;
-var vitesse = 0;
-var hypothenuse = 1;
-
-function render() {
-    var images = document.getElementsByClassName("boat")
-    console.log("updateOffsetY");
-
-    var offset = 0;
-    var totalImgI = 0;
-
-    for (let imageIndex = 0; imageIndex < numb.length; imageIndex++) {
-        console.log("new image");
-
-        offset += offsetList[imageIndex];
-        for (let i = 0; i < numb[imageIndex]; i++) {
-
-            const elem = images[totalImgI];
-            console.log(offset);
-            // elem.style.transform = `translateY(${offset}px)`;
-
-            offset -= 0.5;
-            totalImgI += 1
-        }
-    }
-};
-
-function press(event) {
-
-    if (event.key === "q") {
-        rotation -= 1;
-        // player.style.transform = "rotate(0deg)";
-
-        // render(rotation);
-
-        player.style.transform = "rotate(" + rotation + "deg)";
-        console.log("q", rotation);
-        rotation = rotation;
-
-    }
-
-    else if (event.key === "d") {
-        rotation += 1;
-        // player.style.transform = "rotate(0deg)";
-
-        // render();
-
-        player.style.transform = "rotate(" + rotation + "deg)";
-        console.log("d", rotation);
-        rotation = rotation;
-
-        render(rotation);
-    }
-
-    else if (event.key === "z") {
-        if (vitesse < 5) {
-            vitesse += 1;
-        }
-        // console.log("z",vitesse);
-    }
-};
-
-document.addEventListener("keydown", press);
-
-
-
-function vitesseSlow() {
-    if (vitesse <= 0.01) {
-        vitesse = 0
-        return vitesse
-    }
-    vitesse = Math.round(vitesse * 2 / 3 * 100) / 100; //fois 2/3 puis garde 2 chiffres après la ,
-
-    // console.log("rotation = ", rotation);
-    // console.log("vitesse = ",vitesse);
-};
-
-function gameTick() {
-    vitesseSlow();
-};
-
-setInterval(gameTick, 500);
+button.addEventListener("click", tourner);
